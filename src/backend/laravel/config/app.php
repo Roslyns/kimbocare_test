bc80241d1dff8dbdea5a4be6958f3de7ad4c4762
Roslyns <roslyn.works@gmail.com>
2024-08-01 11:03:59 +0100

    swagger et jwt rajoutes.

--- src/backend/laravel/config/app.php
+++ src/backend/laravel/config/app.php
@@ -168,6 +168,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class, // <--added
+        L5Swagger\L5SwaggerServiceProvider::class, // <--added
     ])->toArray(),
 
     /*
